0a00ef4ac8fae5b4cd8e3de21b60bcfc42679f48
Mark <markmwago@gmail.com>
2019-03-05 15:49:20 +0300

    add changes in JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,16 @@
 
 // user interface
 $(document).ready(function(){
-  $("input[type= 'button']").click(function)(){
+  $("input[type= 'button']").click(function() {
+
+
     var Q1 = parseInt($('input[name = "Q1"]:checked').val());
     var Q2 = parseInt($('input[name = "Q2"]:checked').val());
     var Q3 = parseInt($('input[name = "Q3"]:checked').val());
     var Q4 = parseInt($('input[name = "Q3"]:checked').val());
     var Q5 = parseInt($('input[name = "Q5"]:checked').val());
-  }
-});
+
+
 
 var total = parseInt["Q1+Q2+Q3+Q4+Q5"];
 
@@ -17,15 +19,15 @@ if (total<40) {
     $("#comment1").text('Work harder');
 }
 
-if (total> = 50 && total<90){
+if (total >= 50 && total<90) {
     $('#comment2'). text('Nice try');
 }
 
-if (total>=90){
+if (total>=90) {
   $('#comment3').text('Excellent');
 }
-
-$("#display").text('You scored + total +');
-
-
+alert(total);
+// document.getElementById("results").innerHTML('You scored' + total +'points');
  event.preventDefault();
+   });
+});
